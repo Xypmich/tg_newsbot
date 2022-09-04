ad109f1782446e8d52f3cbd998636c917d0d61f8
Xypmich <kvudev@gmail.com>
2022-09-04 11:23:18 +0300

    initial commit

---
+++ Authorization/tg_web_auth.py
@@ -0,0 +1,17 @@
+from selenium import webdriver
+
+
+class TgWebManip:
+    driver = webdriver.Firefox()
+    driver.maximize_window()
+    driver.get('https://web.telegram.org')
+
+    def phone_num_auth(self):
+        try:
+            TgWebManip.driver.
+        except Exception:
+
+
+
+if __name__ == '__main__':
+    pass
